3cf96fcbf2f2d820dd7b803b5f42b72961818f03
Joan-Abner <joanabner05@gmail.com>
2019-07-22 13:19:37 -0500

    INITIALIZE-PROJECT

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express  = require('express');
+const app = express();
+
+//settings
+
+//Middlewares
+
+//Routes
+
+//Static files
+
+//starting the server
+app.listen(3000, ()  => {
+console.log('server on port 3000');
+});
